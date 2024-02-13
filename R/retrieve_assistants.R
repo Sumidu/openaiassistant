26b821da84b0236276a5b5f970ada6741f88d49a
André Calero Valdez <andrecalerovaldez@gmail.com>
2024-02-13 23:12:51 +0100

    added retrieve assistants call

--- R/retrieve_assistants.R
+++ R/retrieve_assistants.R
@@ -1,27 +1,35 @@
-#' Get list of all assistants
-#' curl "https://api.openai.com/v1/assistants?order=desc&limit=20" \
+#' Retrieve assistant using the API
+#' GET
+#' https://api.openai.com/v1/assistants/{assistant_id}
+#' Retrieves an assistant.
+#' Path parameters
+#' assistant_id
+#' string
+#' Required
+#' The ID of the assistant to retrieve.
+#'
+#' curl https://api.openai.com/v1/assistants/asst_abc123 \
 #' -H "Content-Type: application/json" \
 #' -H "Authorization: Bearer $OPENAI_API_KEY" \
 #' -H "OpenAI-Beta: assistants=v1"
-#' @param order The order to list the assistants in. Options are "asc" and "desc".
-#' @param limit The maximum number of assistants to return.
-#' @retuerns A list of assistant objects
+#' @param assistant_id The ID of the assistant to retrieve.
+#' @returns An assistant object
 #' @export
 #' @examples
-#' list_assistants()
-list_assistants <- function(order = "desc", limit = 20) {
+#' retrieveAssistant("asst_abc123")
+retrieveAssistant <- function(assistant_id){
   check_token()
   base_url <- "https://api.openai.com/"
   req <- httr2::request(base_url)
   resp <- req |>
     httr2::req_url_path_append("v1/assistants") |>
+    httr2::req_url_path_append(assistant_id) |>
     httr2::req_auth_bearer_token(token = Sys.getenv("OPENAI_API_KEY")) |>
     httr2::req_headers("Content-Type" = "application/json") |>
     httr2::req_headers("OpenAI-Beta" = "assistants=v1") |>
-    httr2::req_url_query("order" = order) |>
-    httr2::req_url_query("limit" = limit) |>
     httr2::req_throttle(rate = 15) |>
     httr2::req_perform()
+
 
   httr2::resp_body_json(resp, simplifyVector = TRUE)
 }
