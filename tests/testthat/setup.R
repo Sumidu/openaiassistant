866a684164f559718d8468c9e4ddb58e06a80460
André Calero Valdez <andrecalerovaldez@gmail.com>
2024-02-18 16:04:54 +0100

    fixed offline tests with withr fixture

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,1 +1,6 @@
 library(httptest2)
+test_backup_token <- Sys.getenv("OPENAI_API_KEY")
+Sys.setenv("OPENAI_API_KEY" = "test")
+
+
+withr::defer(Sys.setenv("OPENAI_API_KEY" = test_backup_token), teardown_env())
--- tests/testthat/test-assistant_list.R
+++ tests/testthat/test-assistant_list.R
@@ -1,5 +1,23 @@
 testthat::test_that("List assistants retrieves objects", {
-  lst <- assistant_list()
+
+  fcal <- function(){
+    assistant_list()
+  }
+
+  if(FALSE){
+    # record API response after assistant create and use ID
+    httptest2::capture_requests({
+      resp <- fcal()
+
+    })
+  } else {
+    httptest2::with_mock_api({
+      resp <- fcal()
+    })
+  }
+
+
+  lst <- resp
 
   testthat::expect_type(lst, "list")
 
