c7267162f8dd1529d6d7f3bdc020ae3174acb718
Samuel Tobler <47925314+samueltobler@users.noreply.github.com>
2024-05-14 17:20:29 +0200

    Update assistant_run.R

--- R/assistant_run.R
+++ R/assistant_run.R
@@ -21,12 +21,12 @@ assistant_run <- function(assistant, thread){
   base_url <- "https://api.openai.com/"
   req <- httr2::request(base_url)
   resp <- req |>
-    httr2::req_url_path_append("v1/threads") |>
+    httr2::req_url_path_append("v2/threads") |>
     httr2::req_url_path_append(thread$id) |>
     httr2::req_url_path_append("runs") |>
     httr2::req_auth_bearer_token(token = Sys.getenv("OPENAI_API_KEY")) |>
     httr2::req_headers("Content-Type" = "application/json") |>
-    httr2::req_headers("OpenAI-Beta" = "assistants=v1") |>
+    httr2::req_headers("OpenAI-Beta" = "assistants=v2") |>
     httr2::req_body_json(body) |>
     httr2::req_throttle(rate = 15) |>
     httr2::req_perform()
