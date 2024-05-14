e690c1a5a391bc89c2e2a44aff87efa2211e8f25
Samuel Tobler <47925314+samueltobler@users.noreply.github.com>
2024-05-14 17:23:56 +0200

    Update assistant_retrieve.R

--- R/assistant_retrieve.R
+++ R/assistant_retrieve.R
@@ -13,11 +13,11 @@ assistant_retrieve <- function(assistant_id){
   base_url <- "https://api.openai.com/"
   req <- httr2::request(base_url)
   resp <- req |>
-    httr2::req_url_path_append("v2/assistants") |>
+    httr2::req_url_path_append("v1/assistants") |>
     httr2::req_url_path_append(assistant_id) |>
     httr2::req_auth_bearer_token(token = Sys.getenv("OPENAI_API_KEY")) |>
     httr2::req_headers("Content-Type" = "application/json") |>
-    httr2::req_headers("OpenAI-Beta" = "assistants=v1") |>
+    httr2::req_headers("OpenAI-Beta" = "assistants=v2") |>
     httr2::req_throttle(rate = 15) |>
     httr2::req_perform()
 
