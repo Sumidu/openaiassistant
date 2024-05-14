dc70f16d68725625c4594baa8e8d15a8e8db546f
Samuel Tobler <47925314+samueltobler@users.noreply.github.com>
2024-05-14 17:21:04 +0200

    Update run_retrieve.R

--- R/run_retrieve.R
+++ R/run_retrieve.R
@@ -16,12 +16,12 @@ run_retrieve <- function(run, thread){
   base_url <- "https://api.openai.com/"
   req <- httr2::request(base_url)
   resp <- req |>
-    httr2::req_url_path_append("v1/threads") |>
+    httr2::req_url_path_append("v2/threads") |>
     httr2::req_url_path_append(thread$id) |>
     httr2::req_url_path_append("runs") |>
     httr2::req_url_path_append(run$id) |>
     httr2::req_auth_bearer_token(token = Sys.getenv("OPENAI_API_KEY")) |>
-    httr2::req_headers("OpenAI-Beta" = "assistants=v1") |>
+    httr2::req_headers("OpenAI-Beta" = "assistants=v2") |>
     httr2::req_throttle(rate = 15) |>
     httr2::req_perform()
   httr2::resp_body_json(resp, simplifyVector = TRUE)
