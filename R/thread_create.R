669be74b58df519dd1019b3f8a6cb0cff4537916
Samuel Tobler <47925314+samueltobler@users.noreply.github.com>
2024-05-14 17:22:06 +0200

    Update thread_create.R

--- R/thread_create.R
+++ R/thread_create.R
@@ -12,10 +12,10 @@ thread_create <- function() {
   base_url <- "https://api.openai.com/"
   req <- httr2::request(base_url)
   resp <- req |>
-    httr2::req_url_path_append("v1/threads") |>
+    httr2::req_url_path_append("v2/threads") |>
     httr2::req_auth_bearer_token(token = Sys.getenv("OPENAI_API_KEY")) |>
     httr2::req_headers("Content-Type" = "application/json") |>
-    httr2::req_headers("OpenAI-Beta" = "assistants=v1") |>
+    httr2::req_headers("OpenAI-Beta" = "assistants=v2") |>
     httr2::req_body_json(body) |>
     httr2::req_throttle(rate = 15) |>
     #httr2::req_dry_run()
